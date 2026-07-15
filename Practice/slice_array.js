531ff22ca0c12fa89c030f72c1eec8c07208e406
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-15 23:38:02 +0530

    slice method in array on js

---
+++ Practice/slice_array.js
@@ -0,0 +1,8 @@
+let primary = ["Red", "Yellow", "Blue", "Orange", "Pink", "White"];
+
+console.log(primary.slice());
+console.log(primary.slice(2));
+console.log(primary.slice(2,3));
+console.log(primary.slice(0,6));
+console.log(primary.slice(0,primary.length));
+console.log(primary.slice(-4));
