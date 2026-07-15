41c4c154e9b5436b61ca6c4004dc703b325b1519
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-15 23:28:06 +0530

    concat method in array on js

--- Practice/Concat.js
+++ Practice/Concat.js
@@ -1,5 +1,5 @@
 let primary = ["Red", "Yellow", "Blue"];
 let secondary = ["Orange", "Green", "Violet"]
 
-
-console.log(primary.concat(secondary));
+let allcolors = primary.concat(secondary);
+console.log(allcolors);
