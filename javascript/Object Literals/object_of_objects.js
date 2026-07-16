b0feaa8c758df19bee2385d3a119e24c23c347dc
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-16 18:21:20 +0530

    object of objects in js

---
+++ javascript/Object Literals/object_of_objects.js
@@ -0,0 +1,37 @@
+const classInfo = {
+  aman : {
+    name : "Aman",
+    age : 21,
+    marks : {
+      Phy : 92,
+      Chem : 77,
+      Eng : 84
+    },
+    city : "Kolkata"
+  },
+  shradha : {
+    name : "Shradha",
+    age : 20,
+    marks : {
+      Phy : 82,
+      Chem : 89,
+      Eng : 64
+    },
+    city : "Bhubaneswar"
+  },
+  sarbeswar : {
+    name : "Sarbeswar",
+    age : 20,
+    marks : {
+      Phy : 97,
+      Chem : 43,
+      Eng : 89
+    },
+    city : "Bhubaneswar"
+  }
+};
+console.log(classInfo);
+
+console.log(classInfo.aman);
+console.log(classInfo.shradha);
+console.log(classInfo.sarbeswar);
