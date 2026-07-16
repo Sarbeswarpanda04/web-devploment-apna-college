f6b50df81af9a8e2b8a271ed4a646603159481cb
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-16 11:11:03 +0530

    break

--- Practice/Favorite_movie.js
+++ Practice/Favorite_movie.js
@@ -2,11 +2,14 @@ const fav_mov = "Avatar";
 
 let guess = prompt("Guess my favorite movie : ");
 
-while((guess != fav_mov) && (guess != "quit")){
-      
+while((guess != fav_mov) ){
+      if(guess == "quit"){
+        alert("You quit.");
+        break;
+      }
       guess = prompt("Wrong guess , Please try Again : ");
 }
 
 if(guess == fav_mov){
-  alert("Congratulation 🎉")
+  alert("Congratulation 🎉");
 }
