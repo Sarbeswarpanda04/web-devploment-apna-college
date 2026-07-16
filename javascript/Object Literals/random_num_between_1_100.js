42532d4c4ee99d8bf533b83147263eddd0187ea9
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-16 18:44:01 +0530

    random number betwen 1 to 100

--- javascript/Object Literals/random_num_between_1_100.js
+++ javascript/Object Literals/random_num_between_1_100.js
@@ -1,2 +1,8 @@
 let num = Math.floor(Math.random()*100)+1;
 console.log(num);
+
+let num2 = Math.floor(Math.random()*5)+1;
+console.log(num2);
+
+let num3 = Math.floor(Math.random()*5)+20;
+console.log(num3);
