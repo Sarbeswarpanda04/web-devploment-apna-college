697ee9e0c7ea7f4c6f8556d63ed6ace3a325d311
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-16 18:11:17 +0530

    object literals in js

--- javascript/Object Literals/twitter_post.js
+++ javascript/Object Literals/twitter_post.js
@@ -5,16 +5,34 @@ const post = {
   reposts : 5,
   tags : ["post","firstPost","first"]
 }
-
+console.log("===================");
 console.log(post);
 console.log(post.username);
 console.log(post.content);
 console.log(post.likes);
 console.log(post.reposts);
 console.log(post.tags);
+console.log("===================");
 
 console.log(post["username"]);
 console.log(post["content"]);
 console.log(post["likes"]);
 console.log(post["reposts"]);
 console.log(post["tags"]);
+console.log("===================");
+
+
+console.log(typeof post);
+console.log(typeof post.username);
+console.log(typeof post.content);
+console.log(typeof post.likes);
+console.log(typeof post.reposts);
+console.log(typeof post.tags);
+console.log("===================");
+
+console.log(typeof post["username"]);
+console.log(typeof post["content"]);
+console.log(typeof post["likes"]);
+console.log(typeof post["reposts"]);
+console.log(typeof post["tags"]);
+console.log("===================");
