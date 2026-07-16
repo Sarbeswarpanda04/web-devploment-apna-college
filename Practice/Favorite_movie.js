0db0d0a6b6fdfaae4ef78799c1c88dbe7b7389bd
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-07-16 11:06:44 +0530

    fav movie game

---
+++ Practice/Favorite_movie.js
@@ -0,0 +1,12 @@
+const fav_mov = "Avatar";
+
+let guess = prompt("Guess my favorite movie : ");
+
+while((guess != fav_mov) && (guess != "quit")){
+      
+      guess = prompt("Wrong guess , Please try Again : ");
+}
+
+if(guess == fav_mov){
+  alert("Congratulation 🎉")
+}
