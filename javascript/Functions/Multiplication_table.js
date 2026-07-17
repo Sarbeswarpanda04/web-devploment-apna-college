cb107e4355a40707e87091ddd785abf3df325ed5
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-17 14:30:25 +0530

    Add Multiplication Table

---
+++ javascript/Functions/Multiplication_table.js
@@ -0,0 +1,12 @@
+function MultiplicationTable(n) {
+  for(let i=1 ; i<=10 ; i++){
+  console.log(n+"*"+i+"="+i*n);
+  }
+}
+
+MultiplicationTable(5);
+MultiplicationTable(6);
+MultiplicationTable(7);
+MultiplicationTable(8);
+MultiplicationTable(9);
+MultiplicationTable(10);
