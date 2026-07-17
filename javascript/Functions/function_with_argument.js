79187ca02e9900dd40a74b99f97bd25e4bd1899a
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-17 14:16:39 +0530

    function with argument in js

---
+++ javascript/Functions/function_with_argument.js
@@ -0,0 +1,9 @@
+function arithmatiOperation(a,b) {
+  console.log("a+b=",a+b);
+  console.log("a-b=",a-b);
+  console.log("a*b=",a*b);
+  console.log("a/b=",a/b);
+  console.log("a%b=",a%b);
+}
+
+arithmatiOperation(10,3);
