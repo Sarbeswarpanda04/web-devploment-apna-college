e31d60a9e6b402e9889967a9ea09db4b81f96311
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-17 15:25:35 +0530

    higher order function in js

---
+++ Practice/odd-even-test.js
@@ -0,0 +1,17 @@
+function oddEvenTest(request) {
+    if (request == "odd") {
+        return function (n) {
+            console.log(!(n % 2 == 0));
+        };
+    } else if (request == "even") {
+        return function (n) {
+            console.log(n % 2 == 0);
+        };
+    } else {
+        console.log("wrong request");
+    }
+}
+
+let request = "even";
+let func = oddEvenTest(request); 
+func(6);
