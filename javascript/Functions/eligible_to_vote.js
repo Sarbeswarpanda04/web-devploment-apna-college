5c3b1579a578028be896584538d9a36ebd070c0b
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-17 14:20:59 +0530

    eligible to vote or not in function using js

---
+++ javascript/Functions/eligible_to_vote.js
@@ -0,0 +1,10 @@
+function eligibleToVote(name,age) {
+  if(age>=18){
+    console.log(name,"is eligible to vote");
+  } else {
+    console.log(name,"is not eligible to vote");
+  }
+}
+
+eligibleToVote("Sarbeswar",20);
+eligibleToVote("Soumya",17);
