98896eed9e826eed9b1998592a476ec077b9cc19
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-19 00:12:57 +0530

    Added Set interval functiuon

---
+++ javascript/Functions/set_interval_function.js
@@ -0,0 +1,16 @@
+//setInterval() is used to execute a function repeatedly at a fixed time interval until it is stopped.
+
+let id = setInterval( () => {
+  console.log("Sarbeswar");
+}, 2000);
+
+clearInterval(id); // to stop
+
+
+
+let id2 = setInterval( () => {
+  console.log("How are you ?");
+}, 2000);
+
+clearInterval(id2); // to stop
+
