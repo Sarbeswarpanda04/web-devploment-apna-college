ea525bb12ed6077ad791a0001586dd3a83be8315
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-19 00:58:24 +0530

    reduce method in array

--- javascript/Array Method/reduce.js
+++ javascript/Array Method/reduce.js
@@ -1,7 +1,7 @@
 let nums = [1, 2, 3, 4, 5];
 
-let result = nums.reduce((a, b) => {
+let result = nums.reduce((a, b) => {  // a = accumulator, b = current element
   return a + b;
-});
+},0);  //0 is initial value we can change it 
 
 console.log(result);
