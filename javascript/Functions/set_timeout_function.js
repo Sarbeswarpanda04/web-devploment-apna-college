e8991541b9530dece036264c76712f40446ccb5e
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-19 00:01:16 +0530

    Added set time out funcion in javascript

---
+++ javascript/Functions/set_timeout_function.js
@@ -0,0 +1,9 @@
+//In JavaScript, setTimeout() is used to execute a function once after a specified delay.
+
+console.log("Hi there !");
+
+setTimeout( () => {
+  console.log("Sarbeswar");
+}, 1000)
+
+console.log("How are you ?");
