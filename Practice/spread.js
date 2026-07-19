115e64b30ce19784b9c00ada3aea2dcc2310932f
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-07-19 20:46:39 +0530

    Spread in javascript

---
+++ Practice/spread.js
@@ -0,0 +1,8 @@
+//To expand an iterable into multiple values
+
+let arr = [10, 20, 30, 40, 50];
+
+console.log(...arr);
+console.log(arr);
+console.log(Math.min(...arr));
+console.log(Math.max(...arr));
